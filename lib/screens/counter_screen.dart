e6bdcf858f2e873bb3943c39e88d89b58a4eb9f6
daniel <djuanrique@gmail.com>
2025-03-01 11:07:06 -0500

    cuarto_commit

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 15;
   @override
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
     const fontSize25 = TextStyle(fontSize: 25);
-    int counter = 15;
     return Scaffold(
       appBar: AppBar(
         title: const Text('CounterScreen'),
+        centerTitle: true, //solucion android centrar titulo appBar
         backgroundColor: Colors.lightBlueAccent,
         elevation: 1,
       ),
@@ -31,12 +37,32 @@ class CounterScreen extends StatelessWidget {
           ],
         )),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          counter++;
-          print('Hola: $counter');
-        },
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              counter++;
+              setState(() {});
+            },
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.reset_tv_sharp),
+            onPressed: () {
+              counter = 0;
+              setState(() {});
+            },
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.exposure_minus_1_outlined),
+            onPressed: () {
+              counter--;
+              setState(() {});
+            },
+          ),
+        ],
       ),
     );
   }
